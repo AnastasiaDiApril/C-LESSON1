7d5cf2a14dd26fd5dcc5c35a0c475b7d4c34ced0
Anastasia <anastasia@sdikanskiy.local>
2023-04-24 17:18:04 +0300

    find max in massive by element of massive

---
+++ methodArray_lesson/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 2, 5, 18, 93, 10, 45, 19, 89, 93};
+
+int n = array.Length;
+int find = 93;
+
+int index = 0;
+
+while(index < n){
+
+    if(array[index] == find){
+
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
